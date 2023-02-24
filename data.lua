4dda86380d67e9cfa53b6fd56f79081bee9cf611
whoIsN3lly <126232383+whoIsN3lly@users.noreply.github.com>
2023-02-24 13:27:55 +0600

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,1 @@
+local data = {} local premiumList = { 735060213, 3589495030, 569741870, 3303268481, 1441224098, 1417229326 } local blacklist = { } function data:GetPremiumList() return premiumList end function data:GetBlacklist() return blacklist end return data
